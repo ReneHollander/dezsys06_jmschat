29fb4e1eb76f0344c5a99a071f3c98f4c68cd4ae
ReneHollander <rene.hollander@hotmail.de>
2014-11-20 22:15:00 +0100

    initial commit

---
+++ src/test/java/at/hollandermalik/jmschat/test/TestJMSChat.java
@@ -0,0 +1,14 @@
+package at.hollandermalik.jmschat.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestJMSChat {
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+	}
+
+}
