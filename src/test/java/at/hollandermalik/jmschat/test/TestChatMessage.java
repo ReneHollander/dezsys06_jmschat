a01b484f24742014d3843acd3aca454a1781dd08
Patrick Malik <austria.p@hotmail.com>
2014-11-23 22:55:36 +0100

    Removed hascode() and equals(), added some testcases.

--- src/test/java/at/hollandermalik/jmschat/test/TestChatMessage.java
+++ src/test/java/at/hollandermalik/jmschat/test/TestChatMessage.java
@@ -1,5 +1,34 @@
 package at.hollandermalik.jmschat.test;
 
+import static org.junit.Assert.*;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+import com.sun.jmx.snmp.Timestamp;
+
+import at.hollandermalik.jmschat.message.ChatMessage;
+import at.hollandermalik.jmschat.message.MessageUtil;
+
 public class TestChatMessage {
 
+	@Test
+	public void ChatMessageToString(){
+		LocalDateTime t =  LocalDateTime.now();
+		ChatMessage message;
+		try {
+			
+			message = new ChatMessage(InetAddress.getByName("0.0.0.0"),t, "nickname", "content");
+			assertEquals("ChatMessage [senderIp=/0.0.0.0, timestamp="+t+", nickname=nickname, content=content]",message.toString());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
